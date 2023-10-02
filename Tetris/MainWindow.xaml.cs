3a3904a156186daf541cd3ff37cca32f81855e5d
jankusaki <paulius.pj.jankauskas@gmial.com>
2023-10-02 17:43:52 +0300

    game class update

--- Tetris/MainWindow.xaml.cs
+++ Tetris/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        Models.Game game;
         public MainWindow()
         {
+
             InitializeComponent();
+            game = new Models.Game(20, 10);
             DrawGameGrid();
         }
         public void DrawGameGrid()
@@ -32,24 +35,39 @@ namespace Tetris
             {
                 for (int j = 0; j < 10; j++)
                 {
+                    
+                    
                     Rectangle rectangle = new Rectangle()
                     {
                         Stroke = Brushes.White,
                         StrokeThickness = 1,
-                        Fill = Brushes.Black
+                        Fill = GetStrokeColor(i, j)
                     };
                     Grid.SetRow(rectangle, i);
                     Grid.SetColumn(rectangle, j);
-                    //aksljdflkajsdf
                     GameGrid.Children.Add(rectangle);
                 }
             }
 
         }
+        public Brush GetStrokeColor(int i, int j)
+        {
+            switch (game.GetGameBoardUnit(i, j))
+            {
+                case 0: return Brushes.Cyan;
+                case 1: return Brushes.Blue;
+                case 2: return Brushes.Orange;
+                case 3: return Brushes.Yellow;
+                case 4: return Brushes.Green;
+                case 5: return Brushes.Red;
+                case 6: return Brushes.Purple;
+                default: return Brushes.Black;
+            }
+        }
         public void StartGame(object sender, RoutedEventArgs e)
         {
-            Models.Game game = new Models.Game();
-            game.Start();
+            game.SpawnBlock();
+            DrawGameGrid();
 
         }
         public void ShowControls(object sender, RoutedEventArgs e)
